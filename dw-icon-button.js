5ff8b9b3df5dbd2bb999736a20f6a29ae5394a27
chirag <chirag@dreamworldsolutions.com>
2020-01-25 17:03:38 +0530

    fix: Revert -> remove dw-ripple element (for performance test)
    This reverts commit 6d3c5f0426afc7ea2ad789fc129ba14ea7391c8f.

--- dw-icon-button.js
+++ dw-icon-button.js
@@ -13,6 +13,7 @@ import { styleMap } from 'lit-html/directives/style-map';
 
 //These are the dw element needed by this elemenet
 import '@dreamworld/dw-icon/dw-icon.js';
+import '@dreamworld/dw-ripple/dw-ripple.js';
 import { isTouchDevice } from '@dreamworld/web-util/isTouchDevice';
 
 // These are the dw styles element needed by this element.
@@ -106,6 +107,7 @@ export class DwIconButton extends LitElement {
           ?disabled="${this.disabled}"
           ?active="${this.active}">
         </dw-icon>
+        <dw-ripple unbounded ?disabled="${this.disabled}"></dw-ripple>
       </button>
     `
   }
