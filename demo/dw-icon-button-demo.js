a6cc634bea12fae558d0cda0671c8fb75435f204
shakil-mansuri-dream <53848663+shakil-mansuri-dream@users.noreply.github.com>
2020-08-22 21:33:48 +0530

    feat: use our lit element instead of lit-element base class.

--- demo/dw-icon-button-demo.js
+++ demo/dw-icon-button-demo.js
@@ -8,7 +8,8 @@ Code distributed by Google as part of the polymer project is also
 subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
 */
 
-import { css, LitElement, html, svg } from 'lit-element';
+import { css, html, svg } from 'lit-element';
+import { LitElement } from '@dreamworld/pwa-helpers/lit-element.js';
 
 //These are the dw element needed by this elemenet
 import { DwIcon } from '@dreamworld/dw-icon/dw-icon.js';
