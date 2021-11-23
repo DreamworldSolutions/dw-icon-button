b5869d79ff0312902ed0795ddb4fa15010282b78
Nirmal Baldaniya <nirmalb@codetailor.in>
2021-11-23 15:12:09 +0530

    fix(dw-icon-button): fix tooltip issue in touch device. (#29)
    * fix(dw-icon-button): fix tooltip issue in touch device.

--- dw-icon-button.js
+++ dw-icon-button.js
@@ -216,7 +216,8 @@ export class DwIconButton extends buttonFocus(LitElement) {
           ?disabled="${this.disabled}"></dw-icon>
         </dw-icon>
       </button>
-      ${this.title ? html`
+      
+      ${this.title && !isTouchDevice() ? html`
       <dw-tooltip
         .trigger=${"mouseenter"}
         .forEl=${this}
